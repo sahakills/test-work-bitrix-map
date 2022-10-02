ddb6fe679b2f1fdfdd197a9acd9103ebe9f21f04
sahakills <bykovalexandere@yandex.ru>
2022-10-03 03:40:28 +0500

    кеш

--- local/components/bitrix/map.marks/component.php
+++ local/components/bitrix/map.marks/component.php
@@ -1,13 +1,9 @@
 <? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
-use Bitrix\Main\Data\Cache;
-
-
-//проверяем тип инфоблока
-$this->checkIBlockSetup();
-$arResult["ITEMS"] = $this->getElements();
-
 if ($this->StartResultCache())
 {
-    $this->IncludeComponentTemplate();
+    //проверяем тип инфоблока
+    $this->checkIBlockSetup();
+    $arResult["ITEMS"] = $this->getElements();
 }
+$this->IncludeComponentTemplate();
